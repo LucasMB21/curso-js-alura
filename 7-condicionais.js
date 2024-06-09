7e854899475e402979debfcd3a7829b963dfab89
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-06-09 13:47:03 -0300

    update 7-condicionais.js

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -9,22 +9,38 @@ const listasDeDestinos = new Array (
     `Rio de Janeiro`
 );
 
-const idadeComprador = 17;
-const estaAcompanhada = true;
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listasDeDestinos);
 
-if(idadeComprador >= 18){
-    console.log("Comprador maior de idade");
-    listasDeDestinos.splice(1,1); //removendo item 
-} else if(estaAcompanhada == true){
-        console.log("Comprador está acompanhado");
-        listasDeDestinos.splice(1,1); //removendo item 
-    } else{
-        console.log("Não é maior de idade e não posso vender")
-        ;
-    }
+// if(idadeComprador >= 18){
+//     console.log("Comprador maior de idade");
+//     listasDeDestinos.splice(1,1); //removendo item 
+// } else if(estaAcompanhada == true){
+//     console.log("Comprador está acompanhado");
+//     listasDeDestinos.splice(1,1); //removendo item 
+// } else{
+//     console.log("Não é maior de idade e não posso vender")
+//     ;
+// }
+
+if(
+    idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem!!");
+    listasDeDestinos.splice(2, 1); //removendo item 
+} else {
+console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem!!");
+} else{
+    console.log("Você não pode embarcar");
+}
 
 console.log(listasDeDestinos);
 
